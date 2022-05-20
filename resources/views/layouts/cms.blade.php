f91466017980df847a3d36c720d8e52c3eab5dcf
mfajririki <fajririki07@gmail.com>
2022-05-21 02:55:15 +0700

    cms

--- resources/views/layouts/cms.blade.php
+++ resources/views/layouts/cms.blade.php
@@ -284,7 +284,7 @@
             <strong>Copyright &copy; 2021</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
-                <b>Sistem Informasi UBD</b>
+                <b>Teknik Komputer UBD</b>
             </div>
         </footer>
 
